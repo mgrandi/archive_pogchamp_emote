78d4752ef113145754d989900ccfb075fb461aeb
Mark Grandi <markgrandi@gmail.com>
2021-01-14 16:48:08 -0800

    add option to log to a file

--- archive_pogchamp_emote/main.py
+++ archive_pogchamp_emote/main.py
@@ -22,20 +22,7 @@ def main():
         description="Tool to automate the archival of the daily twitch.tv PogChamp emotes each day",
         epilog="Copyright 2021-01-13 - Mark Grandi")
 
-    # set up logging stuff
-    logging.captureWarnings(True) # capture warnings with the logging infrastructure
-    root_logger = logging.getLogger()
-    logging_formatter = utils.ArrowLoggingFormatter("%(asctime)s %(threadName)-10s %(name)-35s %(levelname)-8s: %(message)s")
-    logging_handler = logging.StreamHandler(sys.stdout)
-    logging_handler.setFormatter(logging_formatter)
-    root_logger.addHandler(logging_handler)
 
-
-    # silence urllib3 (requests) logger because its noisy
-    requests_packages_urllib_logger = logging.getLogger("requests.packages.urllib3")
-    requests_packages_urllib_logger.setLevel("INFO")
-    urllib_logger = logging.getLogger("urllib3")
-    urllib_logger.setLevel("INFO")
 
     # optional arguments, if specified these are the input and output files, if not specified, it uses stdin and stdout
     parser.add_argument("--config-file",
@@ -48,17 +35,42 @@ def main():
         type=utils.isDirectoryType,
         required=True,
         help="the root folder that we put everything else in ")
+    parser.add_argument("--log-to-file",
+        dest="log_to_file",
+        type=utils.isFileType(False),
+        help="save the application log to a file as well as print to stdout")
     parser.add_argument("--verbose", action="store_true", help="Increase logging verbosity")
 
 
     try:
         parsed_args = parser.parse_args()
 
+
+        # set up logging stuff
+        logging.captureWarnings(True) # capture warnings with the logging infrastructure
+        root_logger = logging.getLogger()
+        logging_formatter = utils.ArrowLoggingFormatter("%(asctime)s %(threadName)-10s %(name)-40s %(levelname)-8s: %(message)s")
+        logging_handler = logging.StreamHandler(sys.stdout)
+        logging_handler.setFormatter(logging_formatter)
+        root_logger.addHandler(logging_handler)
+
+
+        # silence urllib3 (requests) logger because its noisy
+        # requests_packages_urllib_logger = logging.getLogger("requests.packages.urllib3")
+        # requests_packages_urllib_logger.setLevel("INFO")
+        # urllib_logger = logging.getLogger("urllib3")
+        # urllib_logger.setLevel("INFO")
+
         # set logging level based on arguments
         if parsed_args.verbose:
             root_logger.setLevel("DEBUG")
         else:
             root_logger.setLevel("INFO")
+
+        if parsed_args.log_to_file:
+            file_handler = logging.FileHandler(parsed_args.log_to_file, mode='a', encoding="utf-8")
+            file_handler.setFormatter(logging_formatter)
+            root_logger.addHandler(file_handler)
 
         root_logger.debug("Parsed arguments: %s", parsed_args)
         root_logger.debug("Logger hierarchy:\n%s", logging_tree.format.build_description(node=None))
--- archive_pogchamp_emote/utils.py
+++ archive_pogchamp_emote/utils.py
@@ -187,3 +187,26 @@ def isDirectoryType(filePath):
 
     return path_resolved
 
+def isFileType(strict=True):
+    def _isFileType(filePath):
+        ''' see if the file path given to us by argparse is a file
+        @param filePath - the filepath we get from argparse
+        @return the filepath as a pathlib.Path() if it is a file, else we raise a ArgumentTypeError'''
+
+        path_maybe = pathlib.Path(filePath)
+        path_resolved = None
+
+        # try and resolve the path
+        try:
+            path_resolved = path_maybe.resolve(strict=strict).expanduser()
+
+        except Exception as e:
+            raise argparse.ArgumentTypeError("Failed to parse `{}` as a path: `{}`".format(filePath, e))
+
+        # double check to see if its a file
+        if strict:
+            if not path_resolved.is_file():
+                raise argparse.ArgumentTypeError("The path `{}` is not a file!".format(path_resolved))
+
+        return path_resolved
+    return _isFileType
