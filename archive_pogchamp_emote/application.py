c2f568d4537899744b94d2ed3e6f40a9afe12ba8
Mark Grandi <markgrandi@gmail.com>
2021-01-19 15:50:18 -0800

    handle when there is no twitch twitter announcement url don't save it via the WBM, don't download it using youtube-dl

--- archive_pogchamp_emote/application.py
+++ archive_pogchamp_emote/application.py
@@ -139,15 +139,19 @@ class Application:
             #########################################################################
             # link to the twitter.com post by the Twitch user account announcing the emote of the day
             #########################################################################
-            logger.info("saving the announcement twitter.com/twitch post via the Wayback Machine")
-            twitch_twitter_post_url_archive = utils.save_archive_of_webpage_in_wbm(
-                emote_config.twitch_twitter_post_url, 1, 1, self.args.no_wbm_save)
+            if emote_config.twitch_twitter_post_url:
+                logger.info("saving the announcement twitter.com/twitch post via the Wayback Machine")
+                twitch_twitter_post_url_archive = utils.save_archive_of_webpage_in_wbm(
+                    emote_config.twitch_twitter_post_url, 1, 1, self.args.no_wbm_save)
 
-            f.write(f"{constants.WPULL_ARGUMENT_WARC_HEADER}\n")
-            f.write(f"{constants.WARC_HEADER_STREAMER_TWICH_TWEET_URL}:{emote_config.twitch_twitter_post_url}\n")
+                f.write(f"{constants.WPULL_ARGUMENT_WARC_HEADER}\n")
+                f.write(f"{constants.WARC_HEADER_STREAMER_TWICH_TWEET_URL}:{emote_config.twitch_twitter_post_url}\n")
 
-            f.write(f"{constants.WPULL_ARGUMENT_WARC_HEADER}\n")
-            f.write(f"{constants.WARC_HEADER_STREAMER_TWICH_TWEET_URL_WBM}:{twitch_twitter_post_url_archive}\n")
+                f.write(f"{constants.WPULL_ARGUMENT_WARC_HEADER}\n")
+                f.write(f"{constants.WARC_HEADER_STREAMER_TWICH_TWEET_URL_WBM}:{twitch_twitter_post_url_archive}\n")
+
+            else:
+                logger.info("Twitch did not post on twitter about today's pogchamp emote, therefore nothing to save via the WBM")
 
             #########################################################################
             # any other links the configuration file says to include as headers (plus the WBM backup)
@@ -215,29 +219,42 @@ class Application:
         #########################################################################
         # save twitter.com/twitch twitter post with youtube-dl
         #########################################################################
-        if emote_config.twitch_twitter_post_is_video:
+
+        # see if twitch actually made an announcement today
+        if emote_config.twitch_twitter_post_url:
+
+            # see if that announcement tweet contained a video
+            if emote_config.twitch_twitter_post_is_video:
 
 
-            logger.info("Downloading the twitter.com/twitch announcement video")
+                logger.info("Downloading the twitter.com/twitch announcement video")
 
-            utils.save_video_with_youtube_dl(emote_config.youtube_dl_output_folder,
-                emote_config.twitch_twitter_post_url,
-                emote_config.ytdl_arguments_file_name,
-                self.args.no_youtube_dl)
+                utils.save_video_with_youtube_dl(emote_config.youtube_dl_output_folder,
+                    emote_config.twitch_twitter_post_url,
+                    emote_config.ytdl_arguments_file_name,
+                    self.args.no_youtube_dl)
 
-            logger.info("video download successful")
+                logger.info("video download successful")
+
+            else:
+                logger.info("config has marked that the Twitch twitter post was not a video, not calling youtube-dl")
+
+                filename = "no_twitch.com_twitter_announcement_video.txt"
+                no_video_txt_path = emote_config.youtube_dl_output_folder / "no_twitch.com_twitter_announcement_video.txt"
+                logger.info("writing `%s`", no_video_txt_path)
+
+                with open(no_video_txt_path, "w", encoding="utf-8") as f:
+                    if emote_config.twitch_twitter_post_url:
+                        # twitter post was a tweet, but not a video
+                        f.write(f"no video because the configuration file specified that the twitter post `{emote_config.twitch_twitter_post_url}` had no video, so we skipped downloading it")
+
+                    else:
+                        f.write(f"No video because twitter did not tweet about today's pogchamp emote today at all")
+                logger.info("writing `%s` was successful", filename)
+
 
         else:
-            logger.info("config has marked that the Twitch twitter post was not a video, not calling youtube-dl")
-
-            filename = "no_twitch.com_twitter_announcement_video.txt"
-            no_video_txt_path = emote_config.youtube_dl_output_folder / "no_twitch.com_twitter_announcement_video.txt"
-            logger.info("writing `%s`", no_video_txt_path)
-
-            with open(no_video_txt_path, "w", encoding="utf-8") as f:
-                f.write(f"no video because the configuration file specified that the twitter post `{emote_config.twitch_twitter_post_url}` had no video, so we skipped downloading it")
-
-            logger.info("writing `%s` was successful", filename)
+            logger.info("there was no Twitch twitter announcement post , so nothing to download with youtube-dl")
 
 
         #########################################################################
